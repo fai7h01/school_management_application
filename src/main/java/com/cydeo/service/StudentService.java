802ee2f3ab8cea9c5eeca9d3303ef2ebaaedb232
Asmik <jasmin_ayr@mail.ru>
2024-05-16 00:42:34 +0200

    StudentService, StudentServiceImpl updated

--- src/main/java/com/cydeo/service/StudentService.java
+++ src/main/java/com/cydeo/service/StudentService.java
@@ -1,7 +1,6 @@
 package com.cydeo.service;
 
 import com.cydeo.entity.Student;
-import com.cydeo.entity.User;
 
 public interface StudentService extends CrudService <Student,String> {
 }
--- src/main/java/com/cydeo/service/impl/StudentServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/StudentServiceImpl.java
@@ -1,9 +1,7 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.entity.Student;
-import com.cydeo.entity.User;
 import com.cydeo.service.StudentService;
-import com.cydeo.service.UserService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
