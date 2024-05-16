003b1836403cd13ab34eb2a4e1e47fbfb30c182b
Dao <dao.dekyi@gmail.com>
2024-05-16 08:20:01 -0700

    Commit Course, Course Converter, Course Service, Course ServiceImpl

---
+++ src/main/java/com/cydeo/service/impl/CourseServiceImpl.java
@@ -0,0 +1,84 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.entity.Course;
+import com.cydeo.service.CourseService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+
+@Service//like @Component - creates bean
+public class CourseServiceImpl extends AbstractMapService <Course, Long> implements CourseService {
+    @Override
+    public Course save(Course course) {
+        //Requirement: Assign new id to the course by UUID library
+        if (course.getCourseId() == null)
+            course.setCourseId(UUID.randomUUID().getMostSignificantBits());
+
+        return super.save(course.getCourseId(), course);
+    }
+
+    @Override
+    public Course findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public List<Course> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void update(Course course) {
+        //before update new course, should we do anything in term of status???
+        super.update(course.getCourseId(), course);
+
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        //- Courses with lessons cannot be deleted. (In that case, the page provide this message “This Course has either one or more than one lessons. Not allowed to delete“.)
+        //- If the course is successfully deleted, show “This Course is successfully deleted“ message in the page.
+        //- Before deleting the course, remove this course from all students (courseStudent)
+
+        // Need lesson info and student info to complete this part
+
+        // Draft of solution
+        // Find the course by id
+        Course course = findById(id);
+        /*
+        // Check if the course exists
+        if (course != null) {
+            // Check if the course has any lessons
+            if (!course.getLessons().isEmpty()) {
+                // Throw an exception or provide a message indicating that the course has lessons
+                throw new IllegalStateException("This Course has one or more lessons. Not allowed to delete.");
+            } else {
+                // Remove the course from all students
+                course.getCourseStudents().forEach(student -> student.getCourses().remove(course));
+                // Delete the course
+                super.deleteById(id);
+                // Show a success message
+                System.out.println("This Course is successfully deleted.");
+            }
+        } else {
+            // Show a message indicating that the course does not exist
+            System.out.println("Course not found with id: " + id);
+        }
+
+    }
+    */
+    }
+}
+
+/*
+CREATE:
+- Assign new id to the course by UUID library.
+- While new course is created, newly created course should assign to all students with status false.==> ENUMS for status??? Status as new variable for Course?
+========
+DELETE:
+- Courses with lessons cannot be deleted. (In that case, the page provide this message “This Course has either one or more than one lessons. Not allowed to delete“.)
+- If the course is successfully deleted, show “This Course is successfully deleted“ message in the page.
+- Before deleting the course, remove this course from all students (courseStudent)
+*/
+
