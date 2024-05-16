09a15bab1b749d64816bff299b8bed1e71cbd29f
hushtar <HUXTAR.EZ@gmail.com>
2024-05-16 16:26:28 +0200

    Initial commit.

---
+++ src/main/java/com/cydeo/entity/Lesson.java
@@ -0,0 +1,22 @@
+package com.cydeo.entity;
+
+import com.cydeo.service.CrudService;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Lesson
+    {
+    private String id;
+    private String name;
+    private String description;
+    private Role instructor;
+    // private Course course;
+    // private Student student;
+    }
