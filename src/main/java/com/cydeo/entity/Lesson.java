e1e2de0d855354f9abbf29d6e27816d8ced5c477
Adela-cloud <dilov222@gmail.com>
2024-05-17 17:39:48 +0300

    lesson entity fields are validated, and lesson service implementations are added.

--- src/main/java/com/cydeo/entity/Lesson.java
+++ src/main/java/com/cydeo/entity/Lesson.java
@@ -1,6 +1,10 @@
 package com.cydeo.entity;
 
 import com.cydeo.service.CrudService;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,13 +14,23 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-
 public class Lesson
     {
     private String id;
+    private Student student;
+
+    @NotBlank(message = "Lesson Name is a required field")
+    @Size(max = 40, min = 2, message = "Lesson Name must be between 2 and 40 characters long")
+    @Pattern(regexp = "([A-Za-z]\\w(?:\\s[A-Za-z]\\w*)*)", message = "Lesson Name must use alphabetic characters with spaces")
     private String name;
+
+    @NotBlank(message = "lesson Description is a required field")
+    @Size(max = 100, min = 5, message = "lesson Description must be between 5 and 100 characters long")
     private String description;
+
+    @NotNull(message = "please select a lesson instructor")
     private Role instructor;
-    // private Course course;
-    // private Student student;
+
+    @NotNull(message = "please select a course")
+    private Course course;
     }
