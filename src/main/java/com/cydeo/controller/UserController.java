1e9e8d30c43c13a353e0ffecd4f10f332b49cf29
mahdia <mahdia.nemati1@gmail.com>
2024-05-20 08:03:03 -0600

    User controller has been updated.

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -8,10 +8,7 @@ import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("/user")
@@ -29,7 +26,7 @@ public class UserController {
     }
 
     @GetMapping("/create")
-    public String createUser(Model model){
+    public String createUser(Model model) {
 
         model.addAttribute("user", new User());
 
@@ -43,8 +40,8 @@ public class UserController {
     }
 
     @PostMapping("/create")
-    public String insertUser(@Valid @ModelAttribute("user")User user, BindingResult bindingResult, Model model){
-        if(bindingResult.hasErrors()){
+    public String insertUser(@Valid @ModelAttribute("user") User user, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
             model.addAttribute("roles", roleService.findAll());
             model.addAttribute("states", State.values());
 
@@ -54,6 +51,44 @@ public class UserController {
         userService.save(user);
 
         return "redirect:/user/create";
+    }
+
+
+    @GetMapping("/delete/{username}")
+    public String deleteUser(@PathVariable("username") String username) {
+
+        userService.deleteById(username);
+
+        return "redirect:/user/create";
+    }
+
+    @GetMapping("/update/{username}")
+    public String updateUser(@PathVariable("username") String username, Model model) {
+
+        model.addAttribute("user", userService.findById(username));
+
+        model.addAttribute("roles", roleService.findAll());
+
+        model.addAttribute("states", State.values());
+
+        return "/user/user-update";
+    }
+
+    @PostMapping("/update")
+    public String updateUser(@Valid @ModelAttribute("user") User user, BindingResult bindingResult, Model model) {
+
+        if (bindingResult.hasErrors()) {
+
+            model.addAttribute("roles", roleService.findAll());
+
+            model.addAttribute("states", State.values());
+
+            return "/user/user-update";
+        }
+        userService.update(user);
+
+        return "redirect:/user/create";
+
     }
 
 
