c18de63c4d997e5b9ee8f85d5c14c53bad313281
Adela-cloud <dilov222@gmail.com>
2024-05-22 13:22:03 +0300

    user story 10: method for last requirement is done.

--- src/main/java/com/cydeo/converter/UserConverter.java
+++ src/main/java/com/cydeo/converter/UserConverter.java
@@ -1,7 +1,26 @@
 package com.cydeo.converter;
 
-public class UserConverter {
+import com.cydeo.entity.User;
+import com.cydeo.service.UserService;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Component;
+
+@ConfigurationPropertiesBinding
+@Component
+public class UserConverter implements Converter<String, User> {
+    UserService userService;
+    public UserConverter(UserService userService) {
+        this.userService = userService;
+    }
 
 
+    @Override
+    public User convert(String source) {
+        if (source == null || source.equals("")) {
+            return null;
+        }
+        return userService.findById(source);
 
+    }
 }
