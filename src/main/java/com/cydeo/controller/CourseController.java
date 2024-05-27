8d726d8fe2e580e0d9a70c6a325ef04a07ecd583
Dao <dao.dekyi@gmail.com>
2024-05-27 09:53:51 -0700

    Commit the changes of Course, CourseController and CourseServiceImpl

--- src/main/java/com/cydeo/controller/CourseController.java
+++ src/main/java/com/cydeo/controller/CourseController.java
@@ -3,8 +3,10 @@ package com.cydeo.controller;
 import com.cydeo.entity.Course;
 import com.cydeo.service.CourseService;
 import com.cydeo.service.UserService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
@@ -28,18 +30,32 @@ public class CourseController {
     }
 
     @PostMapping("/create")
-    public String insertCourse(@ModelAttribute("course") Course course) {
+    public String insertCourse(@Valid @ModelAttribute("course") Course course, BindingResult bindingResult, Model model) {
+        if(bindingResult.hasErrors()){
+            model.addAttribute("managers", userService.findManagers());
+            model.addAttribute("courses", courseService.findAll());
+            return "/course/course-create";
+        }
         //before saving a new course, set all students with status "false" (as it is business requirements, set it in ServiceImp not in this Controller)
+
         courseService.save(course);
         return "redirect:/course/create";
     }
 
 
     //update
+    @PostMapping("/update")
+    public String updateCourse(@ModelAttribute("course") Course course){
+        courseService.update(course);
+        return "redirect:/course/create";
+    }
 
     //delete
+    @GetMapping("/delete/{id}")
+    public String deleteCourse(@PathVariable("id") Long id) {
+        courseService.deleteById(id);
+        return "redirect:/course/create";
+    }
 
 }
-
-
 
